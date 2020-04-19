3e712bca90260e740c484916d59ce877b62713f3
charlie-bennett <35985336+charlie-bennett@users.noreply.github.com>
2020-04-19 13:33:51 -0700

    Update auxillary_io.c

--- auxillary_io.c
+++ auxillary_io.c
@@ -1,1 +1,29 @@
 
+void ADC_init(void)
+{
+  ADMUX |= (1 << REFS0 ); // Set reference to AVCC
+  ADMUX &= ~(1 << REFS1 );
+  ADMUX |= (1 << ADLAR ); // Left adjust the output
+  ADCSRA |= (7 << ADPS0 ); // Set the prescalar to 128
+  ADCSRA |= (1 << ADEN ); // Enable the ADC
+  return; 
+}
+//goes in while loop 
+double ADC_poll(unit8_t channel, char iters)
+{
+  ADMUX |= (channel << MUX0 ); // Set the channel 
+  double runsum = 0; 
+  
+  for (int i = 0; i<iters; i++)
+  {
+    ADCSRA |= (1 << ADSC ); // Start a conversion
+    while ( ADCSRA & (1 << ADSC )); // wait for conversion complete
+    runsum+=(5<<ADCH);
+  }
+  double runsum/iters; 
+  
+  /*
+  Do we need to account for left justify or does prescalar value do this for us? 
+  */
+}
+
