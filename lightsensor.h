dbd26f5f0d1d717adee51e4c5e87718dbdefc035
ataozhou <andrewzh@usc.edu>
2020-05-05 13:41:08 -0700

    fixing .h files

--- lightsensor.h
+++ lightsensor.h
@@ -1,3 +1,3 @@
-extern void push_back_LSFIFO(uint8_t, uint8_t, unit8_t, unit8_t&);
+extern void push_back_LSFIFO(uint8_t, uint8_t, uint8_t, uint8_t*);
 extern uint8_t poll_light_sensor();
 extern float get_light_sensor_moving_average();
