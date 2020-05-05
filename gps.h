724bbbfd9e553bebeaa96d070befe832ef20b145
ataozhou <andrewzh@usc.edu>
2020-05-05 16:09:33 -0700

    fixing gps.h

--- gps.h
+++ gps.h
@@ -1,7 +1,7 @@
 
 extern volatile int8_t buzzerflag;
 extern volatile int8_t readingBluetooth;
-extern uint8_t is_coordinates(const char*);
+extern uint8_t is_coordinates(char*);
 extern void gps_init();
 extern void pollGPS(char);
 extern char* popGPS();
