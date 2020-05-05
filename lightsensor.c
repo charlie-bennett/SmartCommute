8d2c9881404d8c47e9c741508e3dd2fb1b693d00
ataozhou <andrewzh@usc.edu>
2020-05-05 15:50:33 -0700

    fixing lightsensor.c

--- lightsensor.c
+++ lightsensor.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <stdio.h>
+#include "adc.h"
 
 #define LS_FIFO_SIZE 32
 uint8_t LS_WP = 0;
@@ -9,19 +10,21 @@ uint8_t LS_BUFFER[LS_FIFO_SIZE];
 uint8_t i = 0;
 float runsum;
 
+
+void push_back_LSFIFO(uint8_t val, uint8_t buffer, uint8_t BUFFER_SIZE, uint8_t* current_loc)
+{
+
+  buffer[((*current_loc) % BUFFER_SIZE)] = val;
+  (*current_loc)++;
+  return;
+}
+
 uint8_t poll_light_sensor()
 {
   adc_init(0X01);
   rawvalue = adc_sample();
   push_back_LSFIFO(rawvalue, LS_BUFFER, LS_FIFO_SIZE, LS_WP);
 
-}
-void push_back_LSFIFO(uint8_t val, uint8_t buffer, uint8_t BUFFER_SIZE, uint8_t& current_loc)
-{
-
-  buffer[(current_loc % BUFFER_SIZE)] = val;
-  current_loc++;
-  return;
 }
 
 float get_light_sensor_moving_average()
@@ -30,6 +33,6 @@ float get_light_sensor_moving_average()
   {
     runsum += LS_BUFFER[i];
   }
-  if (LS_WP < LS_FIFO_SIZE) return runsum / (float WP);
-  return runsum / (float LS_FIFO_SIZE);
+  if (LS_WP < LS_FIFO_SIZE) return runsum / ((float) WP);
+  return runsum / ((float) LS_FIFO_SIZE);
 }
