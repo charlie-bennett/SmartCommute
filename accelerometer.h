35696c93839d7491afba855ef911fd2f6dbc6f06
Charles Bennett <cfbennet@usc.edu>
2020-05-04 17:36:06 -0700

    simplified fifo

--- accelerometer.h
+++ accelerometer.h
@@ -1,9 +1,8 @@
-float XYZ_BUFFER[XYZ_BUFFER_SIZE][3];
-uint8_t WP = 0;
+
 unit8_t initAccelerometer();
 unit8_t pollAccelerometer();
 unit8_t readAccelerometer(float*);
 void push_back_FIFO(float*, float**, uint8_t, uint8_t&);
-float get_moving_average(float**, unit8_t, unit8_t);
+void get_accelerometer_moving_average(float*);
 
 
---
+++ lightsensor.h
@@ -0,0 +1,3 @@
+void push_back_LSFIFO(uint8_t, uint8_t, unit8_t, unit8_t&);
+uint8_t poll_light_sensor();
+float get_light_sensor_moving_average();
