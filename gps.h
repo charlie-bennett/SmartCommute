255db929ccc1cab4f80cabfa89a3e6102ebfb7e5
ataozhou <andrewzh@usc.edu>
2020-05-06 16:53:50 -0700

    hopefully

--- gps.h
+++ gps.h
@@ -2,7 +2,7 @@
 extern volatile int8_t buzzerflag;
 extern volatile int8_t readingBluetooth;
 extern volatile char lcdSpeed[6];
-extern volatile int8_t speedFlag = 1;
+extern volatile int8_t speedFlag;
 extern uint8_t is_coordinates(char*);
 extern void gps_init();
 extern void pollGPS(char);
